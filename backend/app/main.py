dc0b04664e013b8cbc759f379cd5679e2b1feb60
Vikas Sutar <vikassutarfsd1020@gmail.com>
2026-03-02 21:11:16 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,15 +30,6 @@ def get_projects(db: Session = Depends(get_db)):
     print(projects)
     return projects
 
-# @app.get("/")
-# def home():
-#     return{"message": "API is running"}
-
-# # READ
-# @app.get("/Projects/")
-# def get_projects(db: Session = Depends(get_db)):
-#     return db.query(models.Project).all()
-
 # POST
 @app.post("/Projects", response_model=schemas.ProjectResponse)
 def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
@@ -47,19 +38,6 @@ def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)
     db.commit()
     db.refresh(new_project)
     return new_project
-
-# # PUT
-# @app.put("/Projects/{project_id}")
-# def update_project(project:schemas.ProjectUpdate, db: Session= Depends(get_db)):
-#     db_project= db.query(models.Project)
-#     if not db_project:
-#         return None
-    
-    
-#     db.commit()
-#     db.refresh(db_project)
-#     return db_project
-
 # DELETE
 @app.delete("/Projects/{project_id}")
 def delete_project(project_id: int, db: Session=Depends(get_db)):
